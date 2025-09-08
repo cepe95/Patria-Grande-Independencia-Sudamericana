7296a319be10b345c0956eec0a059dddb5b89f92
cepe95 <aguscepedabarzola@gmail.com>
2025-09-08 01:27:35 -0300

    89
    89

--- Data/TownData.gd
+++ Data/TownData.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name TownData
 
 @export var nombre: String = ""
-@export var tipo: String = "villa" # Ej: villa, ciudad, fortaleza, capital
+@export var tipo: String = "villa" # Ej: villa, ciudad, fortaleza, capital, metropolis
 @export var importancia: int = 1
 @export var estado: String = "neutral"
 @export var faccion: String = "" # Nombre de la facción que lo controla
@@ -12,3 +12,14 @@ class_name TownData
 @export var unidades_sostenibles: Array[String] = []
 @export var oficiales_disponibles: Array[String] = []
 @export var comentario: String = ""
+
+# NUEVOS CAMPOS
+@export var poblacion: int = 0
+@export var crecimiento: int = 0
+@export var manpower_rural: int = 0
+@export var manpower_artesanos: int = 0
+@export var manpower_nobleza: int = 0
+@export var produccion: Dictionary = {}
+@export var consumo: Dictionary = {}
+@export var sostenimiento: Dictionary = {}
+@export var oficiales_autogen: Dictionary = {}
